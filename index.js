4ec819048834e9a4ef134ab701f56d527c094542
Andrew Golightly <andrew@golightlyplus.com>
2016-05-08 21:30:32 +0700

    started using eslint

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const city = 'Cape Town';
+const city = 'Chiang Mai';
 
 const OPENWEATHERMAP_API_KEY = process.env.OPENWEATHERMAP_API_KEY;
 const WEATHER_URL_ENDPOINT = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPENWEATHERMAP_API_KEY}`;
 const axios = require('axios');
 
 axios.get(WEATHER_URL_ENDPOINT)
-.then(function (response) {
+.then((response) => {
   const weatherData = response.data;
   console.log(`The weather in ${weatherData.name} (${weatherData.sys.country}): ${weatherData.weather[0].description}`);
   console.log(`Current temperature: ${weatherData.main.temp} (High: ${weatherData.main.temp_max} Low: ${weatherData.main.temp_min})`);
 })
-.catch(function (response) {
+.catch((response) => {
   console.log(response);
 });
