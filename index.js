1d3f0f98170f8e1f3e4c8ea1c39c5df8afd9434e
Andrew Golightly <support@andrewgolightly.com>
2018-02-18 15:05:49 +1300

    improved error handler

--- index.js
+++ index.js
@@ -7,26 +7,35 @@ const app = express();
 // add a task to their trello card
 app.get('/weather', (req, res) => {
   const city = req.query.text;
-  const WEATHER_URL_ENDPOINT = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPENWEATHERMAP_API_KEY}`;
+  const WEATHER_URL_ENDPOINT = `http://api.openweathermap.org/data/2.5/weather?q=${
+    city
+  }&units=metric&appid=${OPENWEATHERMAP_API_KEY}`;
 
-  axios.get(WEATHER_URL_ENDPOINT)
-  .then((response) => {
-    const weatherData = response.data;
-    res.json({
-      response_type: 'in_channel',
-      text: `The weather currently in ${weatherData.name} (${weatherData.sys.country})`,
-      attachments: [
-        {
-          title: weatherData.weather[0].description,
-          text: `Current temperature: ${weatherData.main.temp} (High: ${weatherData.main.temp_max} Low: ${weatherData.main.temp_min})`,
-          thumb_url: `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`
-        }
-      ]
+  axios
+    .get(WEATHER_URL_ENDPOINT)
+    .then(response => {
+      const weatherData = response.data;
+      res.json({
+        response_type: 'in_channel',
+        text: `The weather currently in ${weatherData.name} (${
+          weatherData.sys.country
+        })`,
+        attachments: [
+          {
+            title: weatherData.weather[0].description,
+            text: `Current temperature: ${weatherData.main.temp} (High: ${
+              weatherData.main.temp_max
+            } Low: ${weatherData.main.temp_min})`,
+            thumb_url: `http://openweathermap.org/img/w/${
+              weatherData.weather[0].icon
+            }.png`
+          }
+        ]
+      });
+    })
+    .catch(error => {
+      res.json({ text: error.response.data.message });
     });
-  })
-  .catch((response) => {
-    res.json({ text: response.data.message });
-  });
 });
 
 // start the server
